6cf797278d68e581a8a8793617c333d8d2fe5421
Trang Luong <luongtrang014@gmail.com>
2022-03-18 03:16:34 +0800

    update

--- script.js
+++ script.js
@@ -111,9 +111,9 @@ const getHolidays = async () => {
   //console.log(inputSearch.value);
   try {
     if (inputSearch.value === "") {
-      var url = `https://holidayapi.com/v1/holidays?pretty&country=${inputCountry.value}&year=${inputYear.value}&month=${inputMonth.value}&day=${inputDay.value}&langguage=${inputLanguage.value}&key=${API_KEY}`;
+      var url = `https://holidayapi.com/v1/holidays?pretty&country=${inputCountry.value}&year=${inputYear.value}&month=${inputMonth.value}&day=${inputDay.value}&language=${inputLanguage.value}&key=${API_KEY}`;
     } else {
-      var url = `https://holidayapi.com/v1/holidays?pretty&year=${inputYear.value}&month=${inputMonth.value}&day=${inputDay.value}&search=${inputSearch.value}&langguage=${inputLanguage.value}&key=${API_KEY}`;
+      var url = `https://holidayapi.com/v1/holidays?pretty&year=${inputYear.value}&month=${inputMonth.value}&day=${inputDay.value}&search=${inputSearch.value}&language=${inputLanguage.value}&key=${API_KEY}`;
     }
     console.log(inputLanguage.value);
     //here is how we add a dynamic value (API KEY) to the url
