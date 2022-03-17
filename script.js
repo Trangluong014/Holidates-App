b680f3578d7c4d6f302684bfffcf74938ca87d13
Trang Luong <luongtrang014@gmail.com>
2022-03-17 14:00:56 +0800

    update readme & js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const API_KEY = "f9f8abfb-4c8e-4a4e-b998-d5c517443821";
 
+const getCountries = async () => {
+  try {
+    const url = `https://holidayapi.com/v1/countries?pretty&key=${API_KEY}`;
+    //here is how we add a dynamic value (API KEY) to the url
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log("data", data); //have a look the retrieved data
+    return data;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+const renderCountries = async () => {
+  try {
+    const data = await getCountries();
+    const countriesList = document.getElementById("countries-list");
+    const ulCountriesList = countriesList.children[2];
+    ulCountriesList.innerHTML = "";
+    data.countries.forEach((country, index) => {
+      const x = document.createElement("li");
+      x.innerHTML = `<div class="bullet">${index + 1}</div>
+            <div class="li-wrapper">
+                <div class="li-title">${country.name}</div>
+                <div>Code: ${country.code}</div>
+            </div>`;
+      ulCountriesList.appendChild(x);
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+document.getElementById("countries-list-btn").addEventListener("click", () => {
+  renderCountries();
+});
